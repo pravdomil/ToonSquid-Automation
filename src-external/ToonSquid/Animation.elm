43fcb33c59b4a7588c705ac8892bea617c8f1975
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-28 19:36:28 +0200

    next

--- src-external/ToonSquid/Animation.elm
+++ src-external/ToonSquid/Animation.elm
@@ -20,40 +20,8 @@ toJson a =
   "na": """ ++ Json.Encode.encode 0 (Json.Encode.string a.name) ++ """,
   "tF": 1,
   "re": { "w": 3840, "h": 2160 },
-  "lO":  """ ++ Json.Encode.encode 0 (Json.Encode.list (\x -> Json.Encode.string (ToonSquid.Element.id x)) a.elements) ++ """,
-  "ly": [
-    {
-      "v": 1,
-      "t": 10,
-      "l": {
-        "V": 3,
-        "Na": "Layer 1",
-        "Id": "73296017-F838-4114-B2B4-1729329B3E7A",
-        "LD": "2023-05-28T16:30:53Z",
-        "dr": [],
-        "v": 2,
-        "DI": null
-      }
-    },
-    {
-      "v": 1,
-      "t": 11,
-      "l": {
-        "if": false,
-        "V": 3,
-        "Na": "Background",
-        "Id": "C01FE01D-1201-4312-BF86-F1A1386D66E2",
-        "LD": "2023-05-28T16:30:53Z",
-        "co": {
-          "v": 1,
-          "k": [{ "vl": { "r": 1, "g": 1, "b": 1 }, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }]
-        },
-        "v": 1,
-        "DI": null,
-        "iS": false
-      }
-    }
-  ],
+  "lO":  """ ++ Json.Encode.encode 0 (Json.Encode.list (\x -> Json.Encode.string (ToonSquid.Element.id x)) (List.reverse a.elements)) ++ """,
+  "ly": [""" ++ String.join "," (List.map ToonSquid.Element.toJson a.elements) ++ """],
   "fr": 24,
   "tH": { "v": 1, "r": [], "c": [] },
   "cL": null,
