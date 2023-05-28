9aada25b9e4872b51ce93c98be280043fc85f45e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-28 23:06:47 +0200

    next

--- src-external/ToonSquid/Layer.elm
+++ src-external/ToonSquid/Layer.elm
@@ -109,6 +109,7 @@ backgroundToJson a =
 type alias Drawing =
     { id : String
     , length : Int
+    , layers : List DrawingLayer
     }
 
 
@@ -121,7 +122,114 @@ drawingToJson layer a =
   "e": """ ++ Json.Encode.encode 0 (Json.Encode.int a.length) ++ """,
   "l": """ ++ Json.Encode.encode 0 (Json.Encode.string (id layer)) ++ """,
   "lMD": "2023-05-28T17:43:29Z",
-  "ly": [],
+  "ly": [""" ++ String.join "," (List.map (\x -> Json.Encode.encode 0 (Json.Encode.string (drawingLayerId x))) a.layers) ++ """],
   "s": 1
+}
+"""
+
+
+
+--
+
+
+type DrawingLayer
+    = Text_ Text
+
+
+drawingLayerId : DrawingLayer -> String
+drawingLayerId a =
+    case a of
+        Text_ b ->
+            b.id
+
+
+drawingLayerToJson : Drawing -> DrawingLayer -> String
+drawingLayerToJson drawing a =
+    case a of
+        Text_ b ->
+            textToJson drawing b
+
+
+
+--
+
+
+type alias Text =
+    { id : String
+    , name : String
+    , text : String
+    }
+
+
+textToJson : Drawing -> Text -> String
+textToJson drawing a =
+    """
+{
+  "v": 1,
+  "t": 2,
+  "l": {
+    "v": 3,
+    "Id": """ ++ Json.Encode.encode 0 (Json.Encode.string a.id) ++ """,
+    "Na": """ ++ Json.Encode.encode 0 (Json.Encode.string a.name) ++ """,
+    "t": """ ++ Json.Encode.encode 0 (Json.Encode.string a.text) ++ """,
+    "DI": """ ++ Json.Encode.encode 0 (Json.Encode.string drawing.id) ++ """,
+    "op": { "v": 1, "k": [{ "vl": 1, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+    "V": 3,
+    "li": { "v": 1, "k": [{ "vl": 1, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+    "eT": { "v": 1, "k": [{ "vl": 1, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+    "bM": 0,
+    "sW": { "v": 1, "k": [{ "vl": 0, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+    "lBM": 0,
+    "fS": { "v": 1, "k": [{ "vl": 100, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+    "tf": {
+      "sX": { "v": 1, "k": [{ "vl": 1, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+      "pi": [164.0380859375, 60],
+      "sY": { "v": 1, "k": [{ "vl": 1, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+      "pX": {
+        "v": 1,
+        "k": [
+          {
+            "vl": { "i": 0, "vl": 1864.5462646484375, "v": 1, "o": 0 },
+            "e": { "md": 2, "tp": 0 },
+            "v": 1,
+            "f": 0
+          }
+        ]
+      },
+      "r": { "v": 1, "k": [{ "vl": 0, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+      "v": 1,
+      "aU": true,
+      "pY": {
+        "v": 1,
+        "k": [
+          {
+            "vl": { "i": 0, "vl": 909.42254638671875, "v": 1, "o": 0 },
+            "e": { "md": 2, "tp": 0 },
+            "v": 1,
+            "f": 0
+          }
+        ]
+      }
+    },
+    "lBW": 0,
+    "wg": 400,
+    "le": { "v": 1, "k": [{ "vl": 0, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }] },
+    "LD": "2023-05-28T17:50:08Z",
+    "al": 0,
+    "co": {
+      "v": 1,
+      "k": [
+        { "vl": { "r": 0, "b": 0, "g": 0, "a": 1 }, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }
+      ]
+    },
+    "ty": "Helvetica",
+    "st": 0,
+    "sC": {
+      "v": 1,
+      "k": [
+        { "vl": { "r": 0, "b": 0, "g": 0, "a": 1 }, "e": { "md": 2, "tp": 0 }, "v": 1, "f": 0 }
+      ]
+    }
+  }
 }
 """
