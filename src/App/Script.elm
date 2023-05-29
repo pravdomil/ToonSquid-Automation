c4155ec4f1c45eb2b6e646a49c70f422a25e5753
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-29 12:14:07 +0200

    next

---
+++ src/App/Script.elm
@@ -0,0 +1,53 @@
+module App.Script exposing (..)
+
+import Regex
+
+
+type alias Script =
+    { scenes : List Scene
+    }
+
+
+fromString : String -> List Scene
+fromString a =
+    String.split "\n" a
+        |> List.map stringToScene
+
+
+
+--
+
+
+type alias Scene =
+    { text : String
+    , length : Maybe Float
+    }
+
+
+stringToScene : String -> Scene
+stringToScene a =
+    let
+        regex : Regex.Regex
+        regex =
+            Regex.fromString "^([\\d]+ )?(.*)$"
+                |> Maybe.withDefault Regex.never
+    in
+    Regex.find regex a
+        |> List.head
+        |> Maybe.andThen
+            (\x ->
+                case x.submatches of
+                    length :: (Just text) :: _ ->
+                        Just
+                            (Scene
+                                text
+                                (length
+                                    |> Maybe.andThen (\x2 -> String.toInt (String.dropRight 1 x2))
+                                    |> Maybe.map (\x2 -> toFloat x2 / 100)
+                                )
+                            )
+
+                    _ ->
+                        Nothing
+            )
+        |> Maybe.withDefault (Scene a Nothing)
